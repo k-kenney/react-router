1361b0330c94441f4f34ba4000842a30e170d146
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-07-30 09:44:43 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider
+} from 'react-router-dom';
+
+// pages
+import Home from './pages/Home'
+import About from './pages/About'
+
+// Layouts
+import RootLayout from './layouts/RootLayout';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={ <Home /> }/>
+      <Route path="about" element={ <About /> }/>
+    </Route>
+  )
+)
+
+function App() {
+  return (
+  <RouterProvider router={router} />
+  );
+}
+
+export default App
